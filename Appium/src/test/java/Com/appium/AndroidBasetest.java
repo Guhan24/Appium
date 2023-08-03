1ed859662b7b9a6a3d3e00f4147bc0e8835c6001
vickypriya <vickypriya@DESKTOP-DN3ISU6>
2023-08-03 13:03:16 +0530

    commit

---
+++ Appium/src/test/java/Com/appium/AndroidBasetest.java
@@ -0,0 +1,81 @@
+package Com.appium;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.testng.annotations.Test;
+
+import com.google.common.collect.ImmutableMap;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+public class AndroidBasetest {
+//	package com.sibibase;
+//
+//	import java.net.MalformedURLException;
+//	import java.net.URL;
+//	import java.time.Duration;
+//
+//	import org.openqa.selenium.JavascriptExecutor;
+//	import org.openqa.selenium.WebElement;
+//	import org.openqa.selenium.remote.RemoteWebElement;
+//	import org.testng.annotations.AfterClass;
+//	import org.testng.annotations.BeforeClass;
+//	import org.testng.annotations.Test;
+//
+//	import com.google.common.collect.ImmutableMap;
+//
+//	import io.appium.java_client.android.AndroidDriver;
+//	import io.appium.java_client.android.options.UiAutomator2Options;
+//
+//	public class AndroidBasetest {
+
+		
+		AndroidDriver androidDriver;
+		
+		//@BeforeClass
+		@Test
+//		public void configureAppium() throws MalformedURLException
+//		{
+		public void configureAppium() throws MalformedURLException {
+		
+			UiAutomator2Options options=new UiAutomator2Options();
+			options.setDeviceName("Mura");
+			//options.setUdid("mvcax8eugesghawg");
+			options.setApp("D:\\UdamyNotes\\appium-master\\appium-master\\AppiumPixel\\src\\test\\java\\utils\\ApiDemos-debug.apk");
+			
+			 androidDriver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), options);
+		
+			 androidDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		}
+		
+		public void longPressAction(WebElement element)
+		{
+			  
+			// Java
+			   ((JavascriptExecutor) androidDriver).executeScript("mobile: longClickGesture", ImmutableMap.of(
+			       "elementId", ((RemoteWebElement) element).getId(), "duration",2000 ));
+		}
+		
+		public void swiping(WebElement element,String string)
+		{
+			((JavascriptExecutor) androidDriver).executeScript("mobile: swipeGesture", ImmutableMap.of(
+				    "elementId",element,
+				    "direction", "left",
+				    "percent", 0.75
+				));
+		}
+//		@AfterClass
+//		public void tearDown()
+//		{
+//			androidDriver.quit();
+//		}
+		
+	}
+
+
